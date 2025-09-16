068ab547edf819b49b73655cd47804d6f572e811
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-09-16 03:29:21 +0300

    factory dp

---
+++ src/Factory/CardTypes.java
@@ -0,0 +1,7 @@
+package Factory;
+
+public enum CardTypes {
+    VISA,
+    MASTERCARD,
+    AMERICANEXPRESS,
+}
---
+++ src/Factory/PaymentProcessor.java
@@ -0,0 +1,23 @@
+package Factory;
+
+public class PaymentProcessor {
+    public void processPayment(String cardType, double amount , String cardHolder , String cardNumber , String cvv , String expiryDate ){
+        //logic of create the real card and processing
+        PaymentMethod paymentMethod = null;
+        if(cardType.equalsIgnoreCase(CardTypes.VISA.toString())){
+            paymentMethod = new Visa(cardHolder,cardNumber,cvv,expiryDate);
+        } else if (cardType.equalsIgnoreCase(CardTypes.MASTERCARD.toString())) {
+            paymentMethod = new MasterCard(cardHolder,cardNumber,cvv,expiryDate);
+        } else if (cardType.equalsIgnoreCase(CardTypes.AMERICANEXPRESS.toString())) {
+            paymentMethod = new AmericanExpress(cardHolder,cardNumber,cvv,expiryDate);
+        }else{
+            throw new IllegalArgumentException("Invalid card type");
+        }
+
+        //process payment
+        paymentMethod.authorizePayment();
+        paymentMethod.startMoneyTransfer();
+        paymentMethod.CalculatePaymentFees(amount);
+    }
+
+}
